1684405c4a7df640669b551d5f84eb95d7539f26
ines <ines@inesitapc>
2020-02-25 19:03:18 +0100

    nuestro proyecto de pogramacion

--- JavaApplication3/src/PokerDeDados/Pantalla.java
+++ JavaApplication3/src/PokerDeDados/Pantalla.java
@@ -1,6 +1,6 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template file, chookjhkjhjkhse Tools | Templates
  * and open the template in the editor.
  */
 package PokerDeDados;
